ee1fb194dbfff4c19f159ec00f55e5d0e1d4b718
karangore-art <karangore603@gmail.com>
2026-02-20 19:55:23 +0530

    Add script.js for portfolio website loading message

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Portfolio Website Loaded Successfully!");
+
+    const name = "Karan Gore";
+    console.log("Welcome to " + name + "'s Portfolio");
+});
